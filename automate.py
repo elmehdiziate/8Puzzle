f55bccce0c904920988d95dea8a12d8f253924e5
elmehdiziate <mziate23@gmail.com>
2023-09-20 16:16:00 +0100

    generate and automate initial states

--- automate.py
+++ automate.py
@@ -2,33 +2,67 @@ import sys
 import csv
 import eightpuzzle 
 import search
+from tabulate import tabulate
 if __name__ == '__main__':
     filename1 = "scenarios.csv"
-    filename2 = "results.csv"
-    # f2 = open(filename2, 'w', newline='')
-    # header = "H3 results"
+    header = ["depth", "expanded_nodes", "max_fringe_size"]
+    table_1 = []
+    table_1.append(["H1 RESULTS"])
+    table_2 = []
+    table_2.append(["H2 RESULTS"])
+    table_3 = []
+    table_3.append(["H3 RESULTS"])
+    table_4 = []
+    table_4.append(["H4 RESULTS"])
     with open(filename1, mode='r')as file:
-
-        # reading the CSV file
         csvReader = csv.reader(file)
         count = 0
-        # displaying the contents of the CSV file
-        # writer = csv.writer(f2)
-        # writer.writerow(header)
-        average = 0
+        average_1 = [0,0,0]
+        average_2 = [0,0,0]
+        average_3 = [0,0,0]
+        average_4 = [0,0,0]
         for lines in csvReader:
             puzzle_list = list(map(lambda n: int(n), lines))
             puzzle = eightpuzzle.EightPuzzleState(list(puzzle_list))
             problem = eightpuzzle.EightPuzzleSearchProblem(puzzle)
             results = search.aStarSearch(problem, search.H1)
-            average += results[2]
+            average_1[0] += results[0]
+            average_1[1] += results[1]
+            average_1[2] += results[2]
+            table_1.append(results)
+            results = search.aStarSearch(problem, search.H2)
+            average_2[0] += results[0]
+            average_2[1] += results[1]
+            average_2[2] += results[2]
+            table_2.append(results)
+            results = search.aStarSearch(problem, search.H3)
+            average_3[0] += results[0]
+            average_3[1] += results[1]
+            average_3[2] += results[2]
+            table_3.append(results)
+            results = search.aStarSearch(problem, search.H4)
+            average_4[0] += results[0]
+            average_4[1] += results[1]
+            average_4[2] += results[2]
+            table_4.append(results)
+            
             count +=1
-        
-            # writer.writerow(list(results))
-    
-    
+            
     # f2.close()
-    print(average/count)
+    table_1.append(["average depth","average expanded nodes", "average fringe size"])
+    table_1.append([average_1[0]/count, average_1[1]/count, average_1[2]/count])
+    print(tabulate(table_1, headers=header,tablefmt="github"))
+    table_2.append(["average depth","average expanded nodes", "average fringe size"])
+    table_2.append([average_2[0]/count, average_2[1]/count, average_2[2]/count])
+    print(tabulate(table_2, headers=header,tablefmt="github"))
+    table_3.append(["average depth","average expanded nodes", "average fringe size"])
+    table_3.append([average_3[0]/count, average_3[1]/count, average_3[2]/count])
+    print(tabulate(table_3, headers=header,tablefmt="github"))
+    table_4.append(["average depth","average expanded nodes", "average fringe size"])
+    table_4.append([average_4[0]/count, average_4[1]/count, average_4[2]/count])
+    print(tabulate(table_4,headers=header,tablefmt="github"))
+
+
     
 
 
--- generate.py
+++ generate.py
@@ -3,25 +3,25 @@ import csv
 from itertools import permutations
 import eightpuzzle as E
 if __name__ == '__main__':
-    numbers = [0,1,2,3,4,5,6,7,8]
-    filename = "permutations.csv"
-    f = open(filename, 'w', newline='')
-    writer = csv.writer(f)
-    all_permutaions = permutations(numbers)
-    
-    for row in all_permutaions:
-        writer.writerow(list(row))
-    # filename = "scenarios.csv"
-    # f = open(filename,'w', newline='')
+    # numbers = [0,1,2,3,4,5,6,7,8]
+    # filename = "permutations.csv"
+    # f = open(filename, 'w', newline='')
     # writer = csv.writer(f)
+    # all_permutaions = permutations(numbers)
     
-    # for i in range(20):
-    #     puzzle = E.createRandomEightPuzzle(25)
-    #     puzzle_list = []
-    #     for i in puzzle.cells:
-    #         for j in i:
-    #             puzzle_list.append(j)
-    #     writer.writerow(puzzle_list)            
+    # for row in all_permutaions:
+    #     writer.writerow(list(row))
+    filename = "scenarios.csv"
+    f = open(filename,'w', newline='')
+    writer = csv.writer(f)
+    
+    for i in range(100):
+        puzzle = E.createRandomEightPuzzle(25)
+        puzzle_list = []
+        for i in puzzle.cells:
+            for j in i:
+                puzzle_list.append(j)
+        writer.writerow(puzzle_list)            
     
         
 
